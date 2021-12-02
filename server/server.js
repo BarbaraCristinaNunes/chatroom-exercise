b9cd84710eb66043d720877ff119ca84a1225f7c
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-02 09:42:28 +0100

    Step 7 is done
    created the const io = require('socket.io')(server);

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 const clientPath = `${__dirname}/../client`;
 app.use(express.static(clientPath));
 const server = http.createServer(app);
+const io = require('socket.io')(server);
 
 server.listen(8080, () =>{
     console.log("server running on "+8080);
