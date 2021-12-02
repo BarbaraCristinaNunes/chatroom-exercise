34481f4ae8c40317bd4acaddcf1e46df07d746d0
BarbaraCristinaNunes <90681011+BarbaraCristinaNunes@users.noreply.github.com>
2021-12-02 11:03:21 +0100

    Step 14 is done
    The code is working

--- client/script.js
+++ client/script.js
@@ -1,1 +1,17 @@
 let socket = io.connect();
+
+let target = document.getElementById("target");
+let message = document.getElementById("message");
+const sendToAll = document.getElementById("sendToAll");
+
+console.log(message)
+
+sendToAll.addEventListener('click', () => {
+    socket.emit('sendToAll', message.value);
+});
+
+socket.on('displayMessage', (message) => {
+    target.innerHTML += '<br>'+message;
+});
+
+
--- server/server.js
+++ server/server.js
@@ -9,9 +9,15 @@ let counter = 0
 
 server.listen(8080, () =>{
     console.log("server running on "+8080);
- });
-
- io.on('connection', (socket) => {
-     counter++;
-    console.log(counter + 'someone connected');
 });
+
+io.on('connection', (socket) => {
+    counter++;
+    console.log(counter + 'someone connected');
+
+    socket.on('sendToAll', (message) =>{
+        console.log("OKOKOK");
+        io.emit("displayMessage", (message));
+    });
+});
+
